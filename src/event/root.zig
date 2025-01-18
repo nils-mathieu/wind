c8584d819f09500ee41cf6fd3a7d81d54c825978
Nils <nils.mathieu.contact@gmail.com>
2025-01-18 16:01:16 +0100

    change some filenames

--- src/event/root.zig
+++ src/event/root.zig
@@ -1,5 +1,4 @@
 pub const Event = @import("Event.zig").Event;
 pub usingnamespace @import("keyboard.zig");
 pub const Ime = @import("Ime.zig").Ime;
-pub usingnamespace @import("mouse.zig");
-pub usingnamespace @import("touch.zig");
+pub usingnamespace @import("pointer.zig");
